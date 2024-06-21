f168fbee6acf861ded778d1ca30f956eb47f3153
Max Kuznetsov <mx.kuznetsov.dev@gmail.com>
2024-06-21 15:10:32 +0300

    move FirebaseApp.configure to applicationDidLaunchWithOptions

--- Sources/UDFAnalyticsFirebase/AnalyticsFirebase.swift
+++ Sources/UDFAnalyticsFirebase/AnalyticsFirebase.swift
@@ -11,13 +11,7 @@ private typealias FAnalytics = FirebaseAnalytics.Analytics
 
 public struct AnalyticsFirebase<Event: RawRepresentable>: UDFAnalytics.Analytics where Event.RawValue == String {
 
-    public init() {
-        if !ProcessInfo.processInfo.xcTest {
-            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")!
-            let options = FirebaseOptions(contentsOfFile: filePath)
-            FirebaseApp.configure(options: options!)
-        }
-    }
+    public init() {}
 
     public func logEvent(_ event: Event) {
         FAnalytics.logEvent(event.rawValue, parameters: nil)
@@ -90,6 +84,10 @@ public struct AnalyticsFirebase<Event: RawRepresentable>: UDFAnalytics.Analytics
         application: UIApplication,
         _ launchOptions: [UIApplication.LaunchOptionsKey : Any]?
     ) {
-        //do nothing
+        if !ProcessInfo.processInfo.xcTest {
+            let filePath = Bundle.main.path(forResource: "GoogleService-Info", ofType: "plist")!
+            let options = FirebaseOptions(contentsOfFile: filePath)
+            FirebaseApp.configure(options: options!)
+        }
     }
 }
